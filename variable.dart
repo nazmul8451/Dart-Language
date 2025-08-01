f0acbbd7287e6f84e69268d857e1a693ca88da65
Rimon <rimon124567@gmail.com>
2025-08-01 15:35:06 +0600

    first dart file and variable dart file

---
+++ variable.dart
@@ -0,0 +1,9 @@
+void main()
+{
+  int rimonAge = 21;
+  String firstName = 'Rimon';
+  String lastName = 'Islam';
+  String department = 'CSE';
+  int rollNo = 683509;
+  print('Hello,$firstName $lastName! Your age is $rimonAge & your department is $department.');
+}
