78f4f5431de78448158ad288df6eaf0b1951c9ad
Rimon <rimon124567@gmail.com>
2025-09-08 07:34:07 +0600

    map_details.dart

--- map_details.dart
+++ map_details.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main(){
   // var map = Map();
   // map['city'] = 'feni';
@@ -13,18 +15,35 @@ void main(){
   //   print({key : map[key]});
   // }
 
-  Map<String,dynamic>users ={
-    'name' : 'rimon',
-    'email' : 'rimon124567@gmail.com',
-    'orders' : [{
-      'order_id' : '12312wqrSqwd21r',
-      'product_name' : ['phone','laptop','watch'],
-      'total' : 25000,
-      'status' : 'pending',
-    }],
+  // Map<String,dynamic>users ={
+  //   'name' : 'rimon',
+  //   'email' : 'rimon124567@gmail.com',
+  //   'orders' : [{
+  //     'order_id' : '12312wqrSqwd21r',
+  //     'product_name' : ['phone','laptop','watch'],
+  //     'total' : 25000,
+  //     'status' : 'pending',
+  //   }],
+  // };
+  // users.addEntries({'city': 'feni'}.entries);
+  // users.forEach((key,value){
+  //   print({key : value});
+  // });
+
+  String input = stdin.readLineSync()!;
+
+  Map<String,int> products = {
+    'I phone 14 pro max' : 98000,
+    'samsung s23 ultra' : 85000,
+    'itel' : 15000,
+    'nokia' : 8000,
+    'xiaomi' : 22000,
   };
-  users.addEntries({'city': 'feni'}.entries);
-  users.forEach((key,value){
-    print({key : value});
-  });
+  input = input.toLowerCase();
+  if(products.containsKey(input)){
+    print('this product is available and price is : ${products[input]} BDT');
+  }else{
+    print('this product is not available');
+  }
+
 }
